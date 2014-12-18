3eb1eee59c5fd99e59e52635f4b6b6c3ea04de99
cbfreeman <cbfreeman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-18 00:05:21 +0000

    4.1 support-dock
    git-svn-id: https://plugins.svn.wordpress.org/support-dock/trunk@1047678 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/sd-tabs-script.js
@@ -0,0 +1,20 @@
+ /**
+ * jQuery Tabs Script v0.1
+ * Client side tabs with jQuery
+ *
+ *
+ */
+   jQuery(document).ready(function() {
+   jQuery( "#tabs" ).tabs();
+  });
+
+
+  
+
+
+
+
+
+  
+
+
